0f8743c3c71fc42f9bbb5148cf33c3fdc5276e3b
Kunamo <98980709+Kunamo@users.noreply.github.com>
2022-06-17 10:50:28 +0200

    Spring and H2 Setup, rn doing CRUD Application

---
+++ src/main/java/ch/elias/safe/SafeApplication.java
@@ -0,0 +1,12 @@
+package ch.elias.safe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SafeApplication.class, args);
+	}
+
+}
